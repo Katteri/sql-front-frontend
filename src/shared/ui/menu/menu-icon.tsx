b4a593b1aa7907af462071d6dd847968b595cfba
Ekaterina Domashova <katy.domashova@gmail.com>
2025-11-12 14:18:46 +0300

    feat: add menu icon component

---
+++ src/shared/ui/menu/menu-icon.tsx
@@ -0,0 +1,20 @@
+import { BLACK_COLOR, RED_COLOR, WHITE_COLOR } from "../colors";
+
+import styles from "./menu.module.scss";
+
+type MenuProps = {
+  color?: typeof BLACK_COLOR | typeof RED_COLOR | typeof WHITE_COLOR,
+};
+
+export const MenuIcon = ({ color=BLACK_COLOR }: MenuProps) => {
+  return (
+    <div
+      className={styles.menu}
+      style={{color}}
+    >
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
