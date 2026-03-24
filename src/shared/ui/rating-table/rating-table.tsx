dc61ffcfcd1ea73168b262dcd10c48d1476470bc
Ekaterina Domashova <katy.domashova@gmail.com>
2026-03-24 12:33:26 +0300

    fix(rating): current user's name

--- src/shared/ui/rating-table/rating-table.tsx
+++ src/shared/ui/rating-table/rating-table.tsx
@@ -2,6 +2,14 @@ import { RatingType } from "@/shared/types/rating-type";
 
 import styles from "./rating-table.module.scss";
 
+const getUserName = ({ fullname, group, login }: Record<string, string>) => {
+  return fullname && group
+    ? `${fullname} ${group}`
+    : fullname 
+    ? fullname
+    : login;
+};
+
 export const RatingTable = ({ data: { currentUser, top_users } }: { data: RatingType }) => {
   const topUsers = top_users
     .filter(({ login }) => login !== currentUser?.login)
@@ -13,26 +21,18 @@ export const RatingTable = ({ data: { currentUser, top_users } }: { data: Rating
         {currentUser && (
           <tr className={styles.highlightedRow}>
             <td>{currentUser.place}</td>
-            <td>{currentUser.login}</td>
+            <td>{getUserName({ fullname: currentUser.fullname, group: currentUser.group, login: currentUser.login })}</td>
             <td>{currentUser.total_score}</td>
           </tr>
         )}
-        {topUsers.map(({ fullname, group, achievement_icons, login, total_score, place }) => {
-          const name = fullname && group
-            ? `${fullname} ${group}`
-            : fullname 
-            ? fullname
-            : login;
-
-          return (
-            <tr key={login}>
-              <td>{place}</td>
-              <td>{name}</td>
-              <td>{achievement_icons ? achievement_icons.map((icon) => (<span key={icon}>{icon}</span>)) : null }</td>
-              <td>{total_score}</td>
-            </tr>
-          );
-        })}
+        {topUsers.map(({ fullname, group, achievement_icons, login, total_score, place }) => (
+          <tr key={login}>
+            <td>{place}</td>
+            <td>{getUserName({ fullname, group, login })}</td>
+            <td>{achievement_icons ? achievement_icons.map((icon) => (<span key={icon}>{icon}</span>)) : null }</td>
+            <td>{total_score}</td>
+          </tr>
+        ))}
       </tbody>
     </table>
   );
